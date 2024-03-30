b1b74ddb9192af772e6105fb260610ed30c9c63b
Bernard Desmyter <bernarddesmyter@gmail.com>
2024-03-30 23:00:13 +0100

    Better jumping for food (no synchronization)

--- Common/world_simulator.cpp
+++ Common/world_simulator.cpp
@@ -169,9 +169,10 @@ namespace darwin {
                 auto sphere = GetSphere(element.physic());
                 if (element.physic().radius() < 50.0) {
                   const auto id = std::hash<std::string>()(element.name());
-                  sphere.x *= 1 + std::abs(0.01*std::sin((2+id%5)*time_));
-                  sphere.y *= 1 + std::abs(0.01*std::sin((2+id%5)*time_));
-                  sphere.z *= 1 + std::abs(0.01*std::sin((2+id%5)*time_));
+                  const double jump_freq = 2 + (id%5000)/1000.;
+                  sphere.x *= 1 + std::abs(0.01*std::sin(jump_freq*time_));
+                  sphere.y *= 1 + std::abs(0.01*std::sin(jump_freq*time_));
+                  sphere.z *= 1 + std::abs(0.01*std::sin(jump_freq*time_));
                 }
                 uniform_enum.spheres.push_back(sphere);
                 uniform_enum.colors.push_back(GetColor(element));
